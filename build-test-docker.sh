d671a50ecb4fc68479717680cd6e2ccd98460498
Juan Carlos García Peláez <juancarlosgpelaez@gmail.com>
2016-01-13 19:47:11 +0000

    Tests for python scripts
    24: Setup of ECS cloudwatch alarms in staging 
    
    Task-Url: http://github.com/addappio/puppet-addapp/issues/issue/24

--- build-test-docker.sh
+++ build-test-docker.sh
@@ -1,3 +1,5 @@
-docker run --rm -v $(pwd):/build -w=/build snelis/tox sh /build/build-test.sh
-
-#docker run --rm -v $(pwd):/build synapse/tox sh /build/build-test.sh
+docker run --rm -v $(pwd):/build -w=/build \
+	-e AWS_ACCESS_KEY_ID \
+	-e AWS_SECRET_ACCESS_KEY \
+	-e AWS_SESSION_TOKEN \
+	snelis/tox sh /build/build-test.sh
