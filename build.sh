91164346786b69cf68099c582034cd369c7c6998
Juan Carlos García Peláez <juancarlosgpelaez@gmail.com>
2016-01-19 08:58:14 +0100

    Update plugins version

--- build.sh
+++ build.sh
@@ -9,4 +9,14 @@ chmod 755 check_*
 fpm -s dir -t deb -n skyscrapers-monitoring-plugins -v ${VERSION} -a all \
   --description "Monitoring plugins that Skyscrapers uses" \
   -x "**/.git/**" -x "**/README.md" -x "**/LICENSE" -x "**/.git" -x "**/build.sh" \
+  -x "**/build" -x "**/dist" -x "**/scripts" -x "**/tests" \
+  -x "**/skymonitoringplugins.egg-info" -x "**/.tox"\
+  -x "*build*.sh" \
+  -x "**/Docker*" \
+  -x "**/.project" \
+  -x "**/tox.ini" \
+  -x "**/circle*" \
+  -x "**/git*" \
+  -x "**/*.pyc" \
+  -x "**/*.py~" \
   .=/usr/lib/nagios/plugins
