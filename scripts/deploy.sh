2e887b3cde204ff9c1150aab6ab3f44e34d1963d
venturel <luca@skyscrapers.eu>
2016-06-30 17:37:20 +0000

    changing path of the key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" # Start the ssh agent
-echo "`ls -la `"
+echo "`sudo ls -la `"
 chmod 600 travis.key # This key should have push access
 scp -i travis.key -o StrictHostKeyChecking=no skyscrapers-monitoring-plugins_*_all.deb travis@repo02.int.skyscrape.rs:
 ssh -i travis.key -o StrictHostKeyChecking=no travis@repo02.int.skyscrape.rs "sudo aptly repo add skyscrapers skyscrapers-monitoring-plugins_*_all.deb;aptly publish update -gpg-key=\"74AD2F75\" skyscrapers s3:repo.int.skyscrape.rs; rm ./skyscrapers-monitoring-plugins_*_all.deb"
