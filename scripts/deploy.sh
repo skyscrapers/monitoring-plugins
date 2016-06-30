9b806ee3954106aabd22f413ba243d4ba9dc2d2b
venturel <luca@skyscrapers.eu>
2016-06-30 18:08:28 +0000

    adding sudo to publish package

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ eval "$(ssh-agent -s)" # Start the ssh agent
 echo "`sudo ls -la `"
 chmod 600 travis.key # This key should have push access
 scp -i travis.key -o StrictHostKeyChecking=no skyscrapers-monitoring-plugins_*_all.deb travis@repo02.int.skyscrape.rs:
-ssh -i travis.key -o StrictHostKeyChecking=no travis@repo02.int.skyscrape.rs "sudo aptly repo add skyscrapers skyscrapers-monitoring-plugins_*_all.deb;aptly publish update -gpg-key=\"74AD2F75\" skyscrapers s3:repo.int.skyscrape.rs; rm ./skyscrapers-monitoring-plugins_*_all.deb"
+ssh -i travis.key -o StrictHostKeyChecking=no travis@repo02.int.skyscrape.rs "sudo aptly repo add skyscrapers skyscrapers-monitoring-plugins_*_all.deb; sudo aptly publish update -gpg-key=\"74AD2F75\" skyscrapers s3:repo.int.skyscrape.rs; rm ./skyscrapers-monitoring-plugins_*_all.deb"
 rm travis.key
