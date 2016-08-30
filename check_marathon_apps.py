c1446f3703b623b32a183c2ccfafcc3b82daef9e
Luca Venturelli <luca@skyscrapers.eu>
2016-08-30 12:23:12 +0200

    adding app_list functionality to add an optional list of variables

--- check_marathon_apps.py
+++ check_marathon_apps.py
@@ -15,22 +15,28 @@ parser.add_argument('-H','--host', help='Marathon hostname', default="10.0.10.11
 parser.add_argument('-p','--port',help='Output file name', default="8080")
 parser.add_argument('-w','--warn',help='% of unhealthy app that is considered a warning', default=20)
 parser.add_argument('-c','--crit',help='% of unhealthy app that is considered critical', default=30)
-parser.add_argument('-a','--app_list',help='List of apps to be monitored comma separated #TODO')
+parser.add_argument('-a','--app_list',help='List of apps to be monitored comma separated')
 parser.add_argument('-W','--unmonit_warn',help='% of app without healthchecks with less task than desired running considered a warning',default=30)
 parser.add_argument('-C','--unmonit_crit',help='% of app without healthchecks with less task than desired running considered critical',default=60)
 
 args = parser.parse_args()
+if args.app_list:
+    app_list=args.app_list.replace(" ","").split(",")
+    
 url="http://"+args.host+":"+args.port+"/v2/apps?embed=apps.counts"
 r=requests.get(url)
 curr_status = 0
 msg=""
-
+apps = []
 if r.status_code != 200:
     msg = "request exited with status code %s" % r.status_code
     curr_status = status["crit"]
 else:
     apps_details=json.loads(r.text)
-    for app in apps_details['apps']:
+    apps=apps_details['apps']
+    if app_list:
+        apps=[app for app in apps_details['apps'] if app["id"] in app_list]
+    for app in apps:
         if app['tasksUnhealthy'] > 0:
             xcent_unhealthy=(float(app["tasksUnhealthy"]/float(app["instances"]))*100)
             msg += "app %s has %s unhealthy tasks; " % (app["id"], app["tasksUnhealthy"])
@@ -49,9 +55,6 @@ else:
                 elif xcent_unhealthy >= args.unmonit_warn:
                     curr_status = status["warn"]
 
-        print("%s tasksUnhealthy %s, tasksHealthy %s, tasksStaged %s, tasksRunning %s, instances %s" % (app["id"], app['tasksUnhealthy'], app['tasksHealthy'], app['tasksStaged'], app['tasksRunning'], app['instances']))
-
-
 if curr_status is status["crit"]:
     print("CRITICAL - " + msg)
     sys.exit(2)
@@ -59,5 +62,5 @@ elif curr_status is status["warn"]:
     print "WARNING - " + msg
     sys.exit(1)
 else:
-    print "OK - all %s apps are running correctly" % len(apps_details['apps'])
+    print "OK - all %s monitored apps are running correctly" % len(apps)
     sys.exit(0)
